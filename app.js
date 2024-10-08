7458ba44d84dce7d312f085e0b97e69a6c4e1c0f
LJS <ljs5303p95@gmail.com>
2024-10-08 20:05:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, './uidprompt.html'));
+   res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(port, () => {
